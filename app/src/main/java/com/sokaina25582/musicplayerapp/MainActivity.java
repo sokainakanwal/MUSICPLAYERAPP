727e60355a39ba265059860187460880513addd5
sokainakanwal <25582@students.riphah.edu.pk>
2023-05-16 17:15:44 -0700

    Init

---
+++ app/src/main/java/com/sokaina25582/musicplayerapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.sokaina25582.musicplayerapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
